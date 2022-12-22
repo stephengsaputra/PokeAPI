108dbd44f3c53aa306af368187606e9c4e6177a6
Stephen Giovanni Saputra <stephen.gsaputra@gmail.com>
2022-12-22 14:33:55 +0700

    Initial commit

--- PokeAPI/ViewController.swift
+++ PokeAPI/ViewController.swift
@@ -9,11 +9,23 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // MARK: - Properties
+    
+    
+    // MARK: - Lifecycle
     override func viewDidLoad() {
+        
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        configureUI()
     }
-
-
+    
+    // MARK: - Selectors
+    
+    
+    // MARK: - Helpers
+    func configureUI() {
+        
+        view.backgroundColor = .systemYellow
+    }
 }
 
